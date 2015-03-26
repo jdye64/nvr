9853df002b7149ec455da7a325e472d0f348a82d
Jeremy Dyer <jdye64@gmail.com>
2015-03-26 15:29:27 -0400

    Bonjour test for locating devices on the local network

---
+++ src/main/java/com/jeremydyer/cli/Bonjour.java
@@ -0,0 +1,58 @@
+package com.jeremydyer.cli;
+
+import com.jeremydyer.NVRConfiguration;
+import io.dropwizard.cli.ConfiguredCommand;
+import io.dropwizard.setup.Bootstrap;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+
+/**
+ * Created by jeremydyer on 3/26/15.
+ */
+public class Bonjour extends ConfiguredCommand<NVRConfiguration> {
+
+    final static Logger logger = LoggerFactory.getLogger(Bonjour.class);
+
+    public Bonjour(String name, String description) {
+        super(name, description);
+    }
+
+    @Override
+    protected void run(Bootstrap<NVRConfiguration> nvrConfigurationBootstrap, Namespace namespace, NVRConfiguration nvrConfiguration) throws Exception {
+        //Create the Bonjour network listener.
+        JmDNS jmDns = JmDNS.create();
+
+        jmDns.addServiceListener("_airplay._tcp.local.", new ServiceListener() {
+            @Override
+            public void serviceAdded(ServiceEvent serviceEvent) {
+
+            }
+
+            @Override
+            public void serviceRemoved(ServiceEvent serviceEvent) {
+
+            }
+
+            @Override
+            public void serviceResolved(ServiceEvent evt) {
+
+                System.out.println();
+                System.out.println("Type: " + evt.getType());
+                System.out.println("Name: " + evt.getName());
+                System.out.println("IP: " + evt.getInfo().getHostAddresses()[0]);
+                System.out.println("Port: " + evt.getInfo().getPort());
+                System.out.println("Name: " + evt.getInfo().getName());
+                System.out.println("Desc: " + evt.getInfo().getNiceTextString());
+                System.out.println("Entry Point: " + evt.getInfo().getURLs()[0]);
+                System.out.println();
+
+            }
+        });
+    }
+}
